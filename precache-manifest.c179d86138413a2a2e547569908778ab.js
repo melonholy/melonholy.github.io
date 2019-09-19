b64f761fb69d18ecd90724325c90fe8b63a61efc
melonholy <36301981+melonholy@users.noreply.github.com>
2019-09-19 10:41:32 +0300

    ыы

---
+++ precache-manifest.c179d86138413a2a2e547569908778ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "344b7221f655e119a7ad91f3489e5d42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64eee012170c730ba6ac",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "48a0fd0367125df72b90",
+    "url": "/static/js/2.c94ba78e.chunk.js"
+  },
+  {
+    "revision": "64eee012170c730ba6ac",
+    "url": "/static/js/main.62b96614.chunk.js"
+  },
+  {
+    "revision": "608567b3a286af875d2a",
+    "url": "/static/js/runtime~main.1dc4db23.js"
+  }
+]);
